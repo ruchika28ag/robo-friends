fc6e0546de81f33778005ea0a9efc2fa42ca93eb
Ruchika Agrawal <rockstarruchi28@gmail.com>
2020-07-13 18:26:18 +0530

    fix container height

--- src/Scroll.js
+++ src/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) =>{
 	return(
-		<div style={{ overflow: 'scroll', border: '5px solid black', height: '477px'}}>
+		<div style={{ overflow: 'auto', height: '75vh'}}>
 			{props.children}
 		</div>
 	);
